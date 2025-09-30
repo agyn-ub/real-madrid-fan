61ab3de01617a33ac6709674766769f98f8ee65c
agyn-ub <agyn.bolatov.1992@gmail.com>
2025-09-30 15:41:02 +0500

    Initial Commit

---
+++ Real Madrid fan/Real_Madrid_fanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Real_Madrid_fanApp.swift
+//  Real Madrid fan
+//
+//  Created by Agyn Bolatov on 30.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Real_Madrid_fanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
